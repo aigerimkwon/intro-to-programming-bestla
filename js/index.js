2a4c077c87db314150543881ddfc1243a4e98843
aigerimkwon <kwonmailbox@gmail.com>
2022-08-10 23:57:07 -0400

    added message form

--- js/index.js
+++ js/index.js
@@ -17,3 +17,32 @@ for (let i = 0; i < skills.length; i ++) {
 }
 
 
+const messageForm = document.getElementsByName("leave_message");
+messageForm.item(0).addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const message = e.target.message.value;
+        console.log(name, email, message);
+
+        const messageSection = document.getElementById('messages');
+        
+        const messageList = messageSection.querySelector('ul');
+        const newMessage = document.createElement('li'); 
+        newMessage.innerHTML = `<a href="mailto: ${email}"> ${name}</a> wrote: <span>${message}</span>`;
+
+        const removeButton = document.createElement('button');
+        removeButton.innerText = "remove";
+        removeButton.type = "button";
+        removeButton.addEventListener('click', (e) => {
+            const entry = removeButton.parentNode;
+            entry.remove();
+        });
+
+        newMessage.appendChild(removeButton); 
+        messageList.appendChild(newMessage);
+         
+        
+
+        messageForm.item(0).reset();
+    });
